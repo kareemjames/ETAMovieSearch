d92e14a3baa24742d995b039753d3d0c269f2a60
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-07 15:00:06 -0500

    create app config

--- src/main/java/com/detroitlabs/moviesearch/AppConfiguration.java
+++ src/main/java/com/detroitlabs/moviesearch/AppConfiguration.java
@@ -1,4 +1,14 @@
 package com.detroitlabs.moviesearch;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
 public class AppConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfiguration.class, args);
+    }
 }
