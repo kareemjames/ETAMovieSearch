fc06df7668094e853475ca54923ad1776c9c7065
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-07 18:24:34 -0500

    add basic movie model

---
+++ src/main/java/com/detroitlabs/moviesearch/model/MovieBasic.java
@@ -0,0 +1,72 @@
+package com.detroitlabs.moviesearch.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MovieBasic {
+
+    private String title;
+    private String year;
+    private String imdbID;
+    private String type;
+    private String poster;
+
+    @Override
+    public String toString() {
+        return "MovieBasic{" +
+                "title='" + title + '\'' +
+                ", year='" + year + '\'' +
+                ", imdbID='" + imdbID + '\'' +
+                ", type='" + type + '\'' +
+                ", poster='" + poster + '\'' +
+                '}';
+    }
+
+    @JsonProperty("Title")
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty("Title")
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @JsonProperty("Year")
+    public String getYear() {
+        return year;
+    }
+
+    @JsonProperty("Year")
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+    @JsonProperty("Type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("Type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("Poster")
+    public String getPoster() {
+        return poster;
+    }
+
+    @JsonProperty("Poster")
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+}
