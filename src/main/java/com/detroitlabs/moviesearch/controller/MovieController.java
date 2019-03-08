d8f349b66c9ad3a7238986d19571ee99fd917646
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-08 15:01:29 -0500

    able to show all movies and movie detail for movie

--- src/main/java/com/detroitlabs/moviesearch/controller/MovieController.java
+++ src/main/java/com/detroitlabs/moviesearch/controller/MovieController.java
@@ -1,10 +1,13 @@
 package com.detroitlabs.moviesearch.controller;
 
+import com.detroitlabs.moviesearch.model.MovieDetails;
 import com.detroitlabs.moviesearch.model.Movies;
 import com.detroitlabs.moviesearch.service.MoviesService;
 import com.detroitlabs.moviesearch.service.MovieDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,6 +26,13 @@ public class MovieController {
     public String displayListOfMovies() {
         Movies allMovies = moviesService.getAllMovies();
         return allMovies.getSearch().toString();
+    }
+
+    @ResponseBody
+    @RequestMapping("/movie/{name}")
+    public String displayMovieDetails(@PathVariable String name, ModelMap modelMap) {
+        MovieDetails movieDetails = movieService.fetchMovieDetails();
+        return movieDetails.toString();
     }
 
 }
