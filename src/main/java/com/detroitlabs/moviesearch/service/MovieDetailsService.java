a3690d684df55bb9d362b5a8d8263609b1f19d0f
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-11 07:42:32 -0400

    change name to imdb and pass it into api call

--- src/main/java/com/detroitlabs/moviesearch/service/MovieDetailsService.java
+++ src/main/java/com/detroitlabs/moviesearch/service/MovieDetailsService.java
@@ -7,19 +7,19 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class MovieDetailsService {
 
-    private String movieName;
+    private String imdbID;
 
     public MovieDetails fetchMovieDetails() {
         RestTemplate restTemplate = new RestTemplate();
 
-        return restTemplate.getForObject("http://www.omdbapi.com/?t=batman&apikey=ea8e0e57", MovieDetails.class);
+        return restTemplate.getForObject("http://www.omdbapi.com/?i=" + imdbID + "&apikey=ea8e0e57", MovieDetails.class);
     }
 
-    public String getMovieName() {
-        return movieName;
+    public String getImdbID() {
+        return imdbID;
     }
 
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
     }
 }
